e02bbd34a4cb66fd5321029e1a830bef546d4564
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2018-10-04 21:50:11 +0800

    Initial Commit

---
+++ ChaChingCurrencyConverter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChaChingCurrencyConverter
+//
+//  Created by Saeed Rahman on 4/10/2018.
+//  Copyright © 2018 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ChaChingCurrencyConverter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChaChingCurrencyConverter
+//
+//  Created by Saeed Rahman on 4/10/2018.
+//  Copyright © 2018 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
